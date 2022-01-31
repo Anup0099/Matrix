7ec8d4a85233f9a4a0fe1a587da98a604d10781f
anup <anuppal602@gmail.com>
2022-01-31 17:04:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol{
+constructor(){
+
+}
+draw(){
+
+}
+}
+
+class Effect {
+constructor(){
+}
+}
