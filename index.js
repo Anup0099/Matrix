f1011509e28dce2bdd84d09d15b370981f96a278
anup <anuppal602@gmail.com>
2022-02-01 22:43:55 +0530

    symbol

--- index.js
+++ index.js
@@ -5,14 +5,48 @@ canvas.height = window.innerHeight;
 
 class Symbol{
 constructor(){
+this.characters='アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+this.x =x;
+this.y =y;
+this.size = size;
+this.text ='';
+this.canvasHeight = canvas.height;
+
 
 }
-draw(){
+draw(context){
+    this.text=this.characters.charAt(Math.floor(Math.random()*this.characters.length));
 
+    context.fillText(this.text,this.x*this.fontSize,this.y*this.fontSize);
+    if(this.y*this.fontSize>this.canvasHeight){
+        this.y=0;
+
+    }else{
+        this.y++;
+    }
 }
 }
 
 class Effect {
-constructor(){
+constructor(canvasWidth, canvasHeight, symbol){
+this.canvasWidth = canvasWidth;
+this.canvasHeight = canvasHeight;
+this.fontSize=25;
+this.column = this.canvasWidth/this.fontSize;
+this.symbol = symbol;
+
+
 }
+#initialize(){
+    for (let index = 0; index < this.column.length; index++) {
+       this.symbol[index] = new Symbol();
+        
+    }
+
+}
+
+}
+
+function animate(){
+
 }
